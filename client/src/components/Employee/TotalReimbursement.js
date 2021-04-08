aeca2dc3036aeb44a643a21f2ba820f28c052e58
Kovarthanan07 <kovarthanan07@gmail.com>
2021-04-09 00:05:25 +0530

    Employee Dashboard Done

--- client/src/components/Employee/TotalReimbursement.js
+++ client/src/components/Employee/TotalReimbursement.js
@@ -1,7 +1,7 @@
 import React from 'react';
-import {Link} from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import { makeStyles } from '@material-ui/core/styles';
-import {Col, Row} from 'reactstrap';
+import { Col, Row } from 'reactstrap';
 import Typography from '@material-ui/core/Typography';
 import Title from '../../components/Title';
 import AOS from 'aos';
@@ -9,46 +9,78 @@ import 'aos/dist/aos.css'; // You can also use <link> for styles
 // ..
 AOS.init();
 
-export default function Deposits() {
+export default function Deposits(props) {
   // const classes = useStyles();
+  const { reimbursements } = props;
+
+  let reimbursementsCopy = [];
+  let pending = [];
+  let approved = [];
+  let rejected = [];
+  let totalreimbursementAmount = 0;
+
+  if (reimbursements) {
+    reimbursementsCopy = reimbursements;
+
+    pending = reimbursements.filter((reimbursement) => {
+      return reimbursement.status === 'Pending';
+    });
+
+    approved = reimbursements.filter((reimbursement) => {
+      return reimbursement.status === 'Done';
+    });
+
+    rejected = reimbursements.filter((reimbursement) => {
+      return reimbursement.status === 'Rejected';
+    });
+
+    reimbursements.map((reimbursement) => {
+      totalreimbursementAmount += reimbursement.amount;
+    });
+  }
+
   return (
     <div>
       <Title>Reimbursement</Title>
       <Row>
         <Col xs={12} sm={8}>
           <Typography component="p" variant="h6">
-          Reimbursement(Rs.):
-      </Typography>
+            Reimbursement(Rs.):
+          </Typography>
         </Col>
         <Col xs={12} sm={4}>
           <Typography component="p" variant="h6">
-            800.00
-      </Typography>
+            {totalreimbursementAmount}
+          </Typography>
         </Col>
       </Row>
       <Row>
         <Col xs={12} sm={8}>
-        <Typography component="p" variant="h6">
-        Total(Number): 
-      </Typography>
+          <Typography component="p" variant="h6">
+            Total(Number):
+          </Typography>
         </Col>
         <Col xs={12} sm={4}>
-        <Typography component="p" variant="h6">
-         10
-      </Typography>
+          <Typography component="p" variant="h6">
+            {reimbursementsCopy.length}
+          </Typography>
         </Col>
       </Row>
       <br />
       <Typography>
         <Row>
           <Col xs={12} sm={4}>
-            <span style={{ color: "#ff6600" }}>Pending: 8</span>
+            <span style={{ color: '#ff6600' }}>Pending: {pending.length}</span>
           </Col>
           <Col xs={12} sm={4}>
-            <span style={{ color: "#00b300" }}>Accepted: 2</span>
+            <span style={{ color: '#00b300' }}>
+              Accepted: {approved.length}
+            </span>
           </Col>
           <Col xs={12} sm={4}>
-            <span style={{ color: "#ff0000" }}>Rejected: 0</span>
+            <span style={{ color: '#ff0000' }}>
+              Rejected: {rejected.length}
+            </span>
           </Col>
         </Row>
       </Typography>
@@ -56,7 +88,9 @@ export default function Deposits() {
       <Typography color="textSecondary">
         until: {new Date().toDateString()}
       </Typography>
-      <Link style={{ textDecoration: "none" }} to="/EmployeeReimburement">SeeMore</Link>
+      <Link style={{ textDecoration: 'none' }} to="/EmployeeReimburement">
+        SeeMore
+      </Link>
     </div>
   );
 }
