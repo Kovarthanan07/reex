fb4e5175a542a472849fa2279cf9483808f4a78c
Vithujan19 <vithusundar@gmail.com>
2021-04-21 23:48:45 +0530

    SearchBtn

--- client/src/components/Staffs/Staffs.js
+++ client/src/components/Staffs/Staffs.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import Sidenav from '../SideNav/Sidenav';
-import { Box, Container } from '@material-ui/core';
+import { Box, Container, Grid } from '@material-ui/core';
+import SearchIcon from '@material-ui/icons/Search';
 import Copyright from '../Footer/Footer';
 import CardView from './CardView';
 import { useStyles } from '../Styles';
+import '../../App.css';
 
 export default function History() {
   const classes = useStyles();
@@ -15,6 +17,16 @@ export default function History() {
       <main className={classes.content}>
         <div className={classes.appBarSpacer} />
         <Container maxWidth="lg" className={classes.container}>
+          <Grid container style={{paddingBottom:"20px"}}>
+            <Grid xs={12} sm={4}></Grid>
+            <Grid xs={12} sm={4}>
+              <div className="staff-search">
+                <input type="text" style={{ backgroundColor: "#fefefe" }} />
+                <SearchIcon />
+              </div>
+            </Grid>
+            <Grid xs={12} sm={4}></Grid>
+          </Grid>
           <CardView />
           <Box pt={4}>
             <Copyright />
